5e046080042650f51e267449c8079acea3aecf9b
Abraham <abraham.tejada@gmail.com>
2025-01-06 21:57:58 -0500

    commit first project

---
+++ test.py
@@ -0,0 +1,17 @@
+cars = [
+    "Toyota Corolla",
+    "Honda Civic",
+    "Ford Mustang",
+    "Chevrolet Tahoe",
+    "Tesla Model 3",
+    "BMW 3 Series",
+    "Audi A4",
+    "Mercedes-Benz C-Class",
+    "Subaru Outback",
+    "Jeep Wrangler",
+    "Hyundai Sonata",
+    "Nissan Altima"
+]
+
+cars_count = 0
+
